6b6adcb8658f347e9af50b8fdd926aab0e9e3c55
Melihcan Sarıkaya <sarikayamelihcan-01@outlook.com>
2023-11-18 12:15:41 +0300

    fragment update

--- app/src/main/java/com/pazarama/moviepicker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pazarama/moviepicker/ui/home/HomeFragment.kt
@@ -1,17 +1,27 @@
 package com.pazarama.moviepicker.ui.home
 
-import androidx.fragment.app.viewModels
-import com.pazarama.moviepicker.base.BaseFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.pazarama.moviepicker.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class HomeFragment @Inject constructor() : BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHomeBinding::inflate) {
+class HomeFragment @Inject constructor() : Fragment() {
 
-    override val viewModel: HomeViewModel by viewModels<HomeViewModel>()
+    private lateinit var _binding: FragmentHomeBinding
+    val binding get() = _binding
 
-
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
 }
