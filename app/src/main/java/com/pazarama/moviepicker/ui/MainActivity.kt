0d8329ba745f4356dc258c779dd3f19d1acf0a7d
Melihcan Sarıkaya <sarikayamelihcan-01@outlook.com>
2023-11-18 11:48:19 +0300

    set nav host fragment

--- app/src/main/java/com/pazarama/moviepicker/ui/MainActivity.kt
+++ app/src/main/java/com/pazarama/moviepicker/ui/MainActivity.kt
@@ -2,13 +2,29 @@ package com.pazarama.moviepicker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.pazarama.moviepicker.R
+import com.pazarama.moviepicker.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setNavHostFragment()
+
+    }
+
+    private fun setNavHostFragment() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.nav_graph)
+        graph.setStartDestination(R.id.homeFragment)
+        val navController = navHostFragment.navController
+        navController.setGraph(graph, intent.extras)
     }
 }
