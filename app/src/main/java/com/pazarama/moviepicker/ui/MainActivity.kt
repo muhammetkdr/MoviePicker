d23fef707541ce13a5f3c6947b9773f77b273819
Muhammet KDR <mskdr1453@gmail.com>
2023-11-18 10:42:00 +0300

    project structure

--- app/src/main/java/com/pazarama/moviepicker/ui/MainActivity.kt
+++ app/src/main/java/com/pazarama/moviepicker/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pazarama.moviepicker
+package com.pazarama.moviepicker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pazarama.moviepicker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
