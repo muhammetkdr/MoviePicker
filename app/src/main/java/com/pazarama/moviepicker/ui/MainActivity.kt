6c716f2145fe3dd011c5d5c5396e21ce7f18c632
Melihcan Sarıkaya <sarikayamelihcan-01@outlook.com>
2023-11-18 11:20:57 +0300

    view binding adding

--- app/src/main/java/com/pazarama/moviepicker/ui/MainActivity.kt
+++ app/src/main/java/com/pazarama/moviepicker/ui/MainActivity.kt
@@ -3,12 +3,19 @@ package com.pazarama.moviepicker.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pazarama.moviepicker.R
+import com.pazarama.moviepicker.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
     }
 }
